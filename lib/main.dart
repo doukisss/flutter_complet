3d180fbc95eaa1e0c8591f3b5cd37c2460cd7503
doukisss <doukissss@gmail.com>
2023-08-28 00:24:57 +0000

    Bottom NavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,23 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        
         colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 92, 1, 248)),
+            ColorScheme.fromSeed(seedColor:const Color.fromARGB(255, 92, 1, 248)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -41,14 +28,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -57,34 +36,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String value = '';
-
-  void show(String a) {
+ var _selectedIndex = 0;
+static const optionalStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+static const List<Widget> _widgetOptions = [
+  Text( 'Index 0: Home', 
+  style: optionalStyle,),
+  Text( 'IndexIndex 1: Business', 
+  style: optionalStyle,),
+  Text( 'Index 2: School',
+  style: optionalStyle,),
+  ];
+  void _onItemTapped(int index){
     setState(() {
-      value = a;
+      _selectedIndex = index; 
     });
   }
-
-  void showx(String a) => setState(() => value = a);
-
-  void showSheet() {
-    showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context) {
-          return Container(
-            padding: const EdgeInsets.all(20),
-            child:const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Text sheet button',
-                  style: TextStyle(color: Colors.red, fontSize: 20),
-                )
-              ],
-            ),
-          );
-        });
-  }
+  @override
 
   @override
   Widget build(BuildContext context) {
@@ -96,41 +63,24 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(color: Colors.black)),
           actions: const <Widget>[Icon(Icons.thumb_up, color: Colors.black)],
         ),
-        persistentFooterButtons: <Widget>[
-          IconButton(
-              onPressed: () => showx('Zoum map'),
-              icon: const Icon(Icons.zoom_out_map)),
-          IconButton(
-              onPressed: () => show('Balance'),
-              icon: const Icon(Icons.account_balance_wallet)),
-          IconButton(
-              onPressed: () => showx('info bank'),
-              icon: const Icon(Icons.add_comment)),
-        ],
+        
+        
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextButton(
-                onPressed: showSheet,
-                child: const Text('Cliquer iCi', style: TextStyle(fontSize: 25),)
-              ),
-              const Text(
-                'Bienvenue chez DoukisssCoder',
-                style: TextStyle(color: Colors.blueGrey, fontSize: 20),
-              ),
-              const Padding(padding: EdgeInsets.only(top: 20)),
-              const Text(
-                'Tuto fluter footer & sheet button',
-                style: TextStyle(color: Colors.green, fontSize: 25),
-              ),
-              const Padding(padding: EdgeInsets.only(top: 20)),
-              Text(
-                value,
-                style: const TextStyle(color: Colors.red, fontSize: 25),
-              )
-            ],
+          child: _widgetOptions.elementAt(_selectedIndex)
           ),
-        ));
+        bottomNavigationBar: BottomNavigationBar(items: const[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.business), label: 'School'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'Business'),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber,
+          onTap: _onItemTapped,
+          ),
+
+        );
+  
+
+
   }
 }

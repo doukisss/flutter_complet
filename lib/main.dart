fc1877bade792a1ef2411afef681819a991b2b61
doukisss <doukissss@gmail.com>
2023-11-10 17:08:49 +0000

    PageView Builder

--- lib/main.dart
+++ lib/main.dart
@@ -50,35 +50,88 @@ class _MyHomePageState extends State<MyHomePage> {
                   actions: const <Widget>[
                     Icon(Icons.thumb_up, color: Colors.green)
                   ],
-                  bottom: const TabBar(tabs: <Widget>[
-                    Tab(
-                      icon: Icon(Icons.ac_unit),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.access_alarm),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.account_balance_sharp),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.account_circle),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.add_task),
-                    ),
-                  ]),
                 ),
-                body: TabBarView(children: <Widget>[
-                  Card(color: Colors.blue, child: Image.asset('images/1.jpeg')),
-                  Card(
-                      color: Colors.green, child: Image.asset('images/2.jpeg')),
-                  Card(color: Colors.red, child: Image.asset('images/3.jpeg')),
-                  Card(
-                      color: Colors.amberAccent,
-                      child: Image.asset('images/4.jpeg')),
-                  Card(
-                      color: Colors.yellow,
-                      child: Image.asset('images/5.jpeg')),
-                ]))));
+                body: PageView.builder(
+                  itemBuilder: (BuildContext buildcontext, int index) =>
+                      Pagewidget(
+                    entry: pages[index],
+                  ),
+                  itemCount: pages.length,
+                ))));
+  }
+}
+
+class Pageinfo {
+  final String title;
+  final String image;
+  final String description;
+  const Pageinfo(this.title, this.image, this.description);
+}
+
+final List<Pageinfo> pages = [
+  const Pageinfo('LE MEILLEUR MODELE N°1', 'images/1.jpeg',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+  const Pageinfo('LE MEILLEUR MODELE N°2', 'images/2.jpeg',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+  const Pageinfo('LE MEILLEUR MODELE N°3', 'images/3.jpeg',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+  const Pageinfo('LE MEILLEUR MODELE N°4', 'images/4.jpeg',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+  const Pageinfo('LE MEILLEUR MODELE N°5', 'images/5.jpeg',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+  const Pageinfo('LE MEILLEUR MODELE N°6', 'images/6.jpeg',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+  const Pageinfo('LE MEILLEUR MODELE N°7', 'images/7.jpeg',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+  const Pageinfo('LE MEILLEUR MODELE N°8', 'images/8.jpeg',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+  const Pageinfo('LE MEILLEUR MODELE N°9', 'images/9.jpeg',
+      'Lorem ipsum dolor sit amet, consectetur adip9scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+  const Pageinfo('LE MEILLEUR MODELE N°10', 'images/10.jpeg',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+];
+
+class Pagewidget extends StatefulWidget {
+  final Pageinfo entry;
+  const Pagewidget({super.key, required this.entry});
+
+  @override
+  State<Pagewidget> createState() => _PagewidgetState();
+}
+
+class _PagewidgetState extends State<Pagewidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text(
+                  widget.entry.title,
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 25,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 10),
+            Image.asset(
+              widget.entry.image,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 10),
+            Text(
+              widget.entry.description,
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ));
   }
 }

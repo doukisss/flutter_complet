338398730d11f74a70bc4fe28bc3746f30e9f76b
doukisss <doukissss@gmail.com>
2023-11-21 19:24:24 +0000

    Form Validator

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -36,45 +37,27 @@ class _MyHomePageState extends State<MyHomePage> {
   //static const optionalStyle = TextStyle(
   //  color: Colors.blueGrey, fontSize: 30, fontWeight: FontWeight.w700);
 
-  late List<Geo> geos;
+  String value = '';
+  String name = '';
+  String phone = '';
+  String password = '';
+
+  final formkey = new GlobalKey<FormState>();
+
   @override
-  void initState() {
-    super.initState();
-    geos = Geo.geolist();
+  void affichage(String a) {
+    setState(() {
+      value = 'Bienvenue $a';
+    });
   }
 
-  Widget tablegeo() {
-    return DataTable(
-        columns: const [
-          DataColumn(
-              label: Text(
-            "Pays",
-            style: TextStyle(color: Colors.blue),
-          )),
-          DataColumn(
-              label: Text(
-            "Capitale",
-            style: TextStyle(color: Colors.blue),
-          )),
-          DataColumn(
-              label: Text(
-            "Continent",
-            style: TextStyle(color: Colors.blue),
-          ))
-        ],
-        rows: geos
-            .map((geo) => DataRow(cells: [
-                  DataCell(
-                    Text(geo.pays),
-                  ),
-                  DataCell(
-                    Text(geo.capitale),
-                  ),
-                  DataCell(
-                    Text(geo.continent),
-                  ),
-                ]))
-            .toList());
+  validationform() {
+    if (formkey.currentState!.validate()) {
+      formkey.currentState!.save();
+      debugPrint(name);
+      debugPrint(phone);
+      debugPrint(password);
+    } else {}
   }
 
   @override
@@ -83,46 +66,87 @@ class _MyHomePageState extends State<MyHomePage> {
         home: DefaultTabController(
             length: 5,
             child: Scaffold(
-                appBar: AppBar(
-                  centerTitle: true,
-                  backgroundColor: Colors.blue,
-                  title: const Text("Tutoriel flutter",
-                      style: TextStyle(color: Colors.white)),
-                  actions: const <Widget>[
-                    Icon(Icons.thumb_up, color: Colors.green)
-                  ],
-                ),
-                body: Center(
-                  child: ListView(
-                    children: [tablegeo()],
+              appBar: AppBar(
+                centerTitle: true,
+                backgroundColor: Colors.blue,
+                title: const Text("Tutoriel flutter",
+                    style: TextStyle(color: Colors.white)),
+                actions: const <Widget>[
+                  Icon(Icons.thumb_up, color: Colors.green)
+                ],
+              ),
+              body: Form(
+                key: formkey,
+                child: Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        value,
+                        style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: 'Nom',
+                            hintText: 'inserer votre nom ici',
+                            icon: Icon(
+                              Icons.person,
+                              size: 25,
+                              color: Colors.blue,
+                            )),
+                        validator: (val) => '$val'.isEmpty
+                            ? 'veillez renseignez votre nom'
+                            : null,
+                        onSaved: (val) => name = '$val',
+                        keyboardType: TextInputType.text,
+                        autocorrect: true,
+                        autofocus: true,
+                        onChanged: affichage,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: 'Numero',
+                            hintText: 'inserer votre phone ici',
+                            icon: Icon(
+                              Icons.person,
+                              size: 25,
+                              color: Colors.blue,
+                            )),
+                        validator: (val) => '$val'.isEmpty
+                            ? 'veillez renseignez votre phone'
+                            : null,
+                        onSaved: (val) => phone = '$val',
+                        keyboardType: TextInputType.phone,
+                        autocorrect: true,
+                        autofocus: true,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: 'Mot de passe',
+                            hintText: 'inserer votre mot de passe ici',
+                            icon: Icon(
+                              Icons.person,
+                              size: 25,
+                              color: Colors.blue,
+                            )),
+                        validator: (val) => '$val'.isEmpty
+                            ? 'veillez renseignez votre mot de passe'
+                            : null,
+                        onSaved: (val) => password = '$val',
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        autocorrect: true,
+                        autofocus: true,
+                      ),
+                      TextButton(
+                          onPressed: validationform, child: Text('Valider'))
+                    ],
                   ),
-                ))));
-  }
-}
-
-class Geo {
-  String pays;
-  String capitale;
-  String continent;
-  Geo({required this.pays, required this.capitale, required this.continent});
-
-  static List<Geo> geolist() {
-    return <Geo>[
-      Geo(pays: "Canada", capitale: "Toronto", continent: "Amerique"),
-      Geo(pays: "USA", capitale: "Washington", continent: "Amerique"),
-      Geo(pays: "Mexique", capitale: "Mexico", continent: "Amerique"),
-      Geo(pays: "Brésil", capitale: "Rio", continent: "Amerique"),
-      Geo(pays: "France", capitale: "Paris", continent: "Europe"),
-      Geo(pays: "Belgique", capitale: "Luxembourg", continent: "Europe"),
-      Geo(pays: "Italie", capitale: "Rome", continent: "Europe"),
-      Geo(pays: "Espagne", capitale: "Madrid", continent: "Europe"),
-      Geo(
-          pays: "Côte D'Ivoire",
-          capitale: "Yamoussoukro",
-          continent: "Afrique"),
-      Geo(pays: "Ghana", capitale: "Accra", continent: "Afrique"),
-      Geo(pays: "Mali", capitale: "Bamako", continent: "Afrique"),
-      Geo(pays: "Guinée", capitale: "Conakri", continent: "Afrique"),
-    ];
+                ),
+              ),
+            )));
   }
 }

426d9fc22d35245ea5860a6acba5c6404e645369
doukisss <doukissss@gmail.com>
2023-12-20 17:29:13 +0000

    DropdownButton

--- lib/main.dart
+++ lib/main.dart
@@ -41,81 +41,70 @@ class _MyHomePageState extends State<MyHomePage> {
   //static const optionalStyle = TextStyle(
   //  color: Colors.blueGr jhey, fontSize: 30, fontWeight: FontWeight.w700);
 
-  int initstep = 0;
+  List<DropdownMenuItem<String>>? listmonth = [];
+  String def = 'Janvier';
 
-  List<Step> steps = [
-    const Step(
-      title: Text(
-        "Veuillez confirmer votre nom",
-        style: TextStyle(color: Colors.blue),
+  void months() {
+    listmonth!.clear();
+    listmonth!.add(
+      const DropdownMenuItem(
+        value: 'Janvier',
+        child: Text(
+          "Janvier",
+          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+        ),
       ),
-      content: Text(
-        "Doukiss Coder",
-        style: TextStyle(color: Colors.blue),
+    );
+    listmonth!.add(
+      const DropdownMenuItem(
+        value: 'Février',
+        child: Text(
+          "Février",
+          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+        ),
       ),
-      subtitle: Text(
-        "Etape 1",
-        style: TextStyle(color: Colors.blue),
+    );
+    listmonth!.add(
+      const DropdownMenuItem(
+        value: 'Mars',
+        child: Text(
+          "Mars",
+          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+        ),
       ),
-    ),
-    const Step(
-      title: Text(
-        "Veuillez confirmer votre numéro",
-        style: TextStyle(color: Colors.blue),
+    );
+    listmonth!.add(
+      const DropdownMenuItem(
+        value: 'Avril',
+        child: Text(
+          "Avril",
+          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+        ),
       ),
-      content: Text(
-        "0107080904",
-        style: TextStyle(color: Colors.blue),
+    );
+    listmonth!.add(
+      const DropdownMenuItem(
+        value: 'Mai',
+        child: Text(
+          "Mai",
+          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+        ),
       ),
-      subtitle: Text(
-        "Etape 2",
-        style: TextStyle(color: Colors.blue),
+    );
+    listmonth!.add(
+      const DropdownMenuItem(
+        value: 'Juin',
+        child: Text(
+          "Juin",
+          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+        ),
       ),
-    ),
-    const Step(
-      title: Text(
-        "Veuillez confirmer votre adresse",
-        style: TextStyle(color: Colors.blue),
-      ),
-      content: Text(
-        "12 bp 1367 Abidjan12",
-        style: TextStyle(color: Colors.blue),
-      ),
-      subtitle: Text(
-        "Etape 3",
-        style: TextStyle(color: Colors.blue),
-      ),
-    ),
-    const Step(
-      title: Text(
-        "Veuillez confirmer votre sexe",
-        style: TextStyle(color: Colors.blue),
-      ),
-      content: Text(
-        "Masculin",
-        style: TextStyle(color: Colors.blue),
-      ),
-      subtitle: Text(
-        "Etape 4",
-        style: TextStyle(color: Colors.blue),
-      ),
-    ),
-    Step(
-      title: Text(
-        "MERCI",
-        style: TextStyle(color: Colors.blue),
-      ),
-      content: Image.network(
-          "https://media.istockphoto.com/id/1262143374/photo/holding-the-paper-with-thank-you-message-in-front-of-a-beautiful-blur-nature-background.jpg?b=1&s=612x612&w=0&k=20&c=T1AINhaJh43Jh3kbUQxMsemAI01mXOpw6vbWrgLqUyo="),
-      subtitle: Text(
-        "Doukiss Coder",
-        style: TextStyle(color: Colors.blue),
-      ),
-    )
-  ];
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    months();
     return MaterialApp(
         home: DefaultTabController(
             length: 5,
@@ -129,35 +118,39 @@ class _MyHomePageState extends State<MyHomePage> {
                     Icon(Icons.thumb_up, color: Color.fromRGBO(76, 175, 80, 1))
                   ],
                 ),
-                body: Stepper(
-                  currentStep: initstep,
-                  steps: steps,
-                  type: StepperType.vertical,
-                  onStepTapped: (Step) {
-                    setState(() {
-                      initstep = Step;
-                    });
-                  },
-                  onStepContinue: () {
-                    setState(() {
-                      if (initstep < steps.length - 1) {
-                        initstep = initstep + 1;
-                      } else {
-                        initstep = 0;
-                      }
-                    });
-                  },
-                  onStepCancel: () {
-                    setState(() {
-                      if (initstep > 0) {
-                        initstep = initstep - 1;
-                      } else {
-                        initstep = 0;
-                      }
-                    });
-                  },
-                )
-                //TabBarView(     )
-                )));
+                body: Container(
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Center(
+                          child: DropdownButton(
+                              value: def,
+                              elevation: 10,
+                              hint: Text(
+                                "Selectionner votre mois",
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30),
+                              ),
+                              items: listmonth,
+                              onChanged: (value) {
+                                def = value!;
+                                setState(() {});
+                              }),
+                        ),
+                        Container(
+                          child: Text(
+                            "Le mois selectionner est: $def",
+                            style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ))));
   }
 }

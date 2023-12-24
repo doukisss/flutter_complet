fad8ea1f2d47c63b326e2e2ffb9af0cb9d2403e9
doukisss <doukissss@gmail.com>
2023-12-24 22:21:39 +0000

    GestureDetector

--- lib/main.dart
+++ lib/main.dart
@@ -33,67 +33,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Widget hotels(String hotelname, image, Color color, double rate) {
-    return Container(
-      child: Column(
-        children: [
-          Image.network(
-            image,
-            fit: BoxFit.fill,
-            height: 300,
-            width: 350,
-          ),
-          Container(
-            child: Row(
-              children: [
-                Text(
-                  hotelname,
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                ),
-                StarRating(
-                  length: 5,
-                  between: 5,
-                  color: color,
-                  rating: rate,
-                  starSize: 30,
-                  onRaitingTap: (rate) {
-                    setState(() {
-                      if (color == Colors.blue) {
-                        rate1 = rate;
-                      }
-                      if (color == Colors.red) {
-                        rate2 = rate;
-                      }
-                      if (color == Colors.green) {
-                        rate3 = rate;
-                      }
-                      if (color == Colors.black) {
-                        rate4 = rate;
-                      }
-                      if (color == Colors.purple) {
-                        rate5 = rate;
-                      }
-                      if (color == Colors.orange) {
-                        rate6 = rate;
-                      }
-                    });
-                  },
-                ),
-                Text("$rate / 5")
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+  String info = '';
+  bool change = true;
+
+  String a = 'images/boisson.jpeg';
+  String b = 'images/pizza.jpeg';
+  String c = 'images/poulet.jpeg';
+  String d = 'images/viande.jpeg';
+
+  void onsubmit(String value) {
+    setState(() {
+      info = value;
+    });
   }
 
-  double rate1 = 3;
-  double rate2 = 2;
-  double rate3 = 4;
-  double rate4 = 5;
-  double rate5 = 1;
-  double rate6 = 4;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -104,42 +57,89 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(color: Colors.black)),
           actions: const <Widget>[Icon(Icons.thumb_up, color: Colors.black)],
         ),
-        body: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            padding: EdgeInsets.all(10),
+        body: Center(
+          child: Container(
             child: Column(
               children: [
-                hotels(
-                    "Onomo hotel",
-                    "https://cf.bstatic.com/xdata/images/hotel/square200/437933425.webp?k=ad3ccee09f58b1b4bd2aee4c392c57ec2ad8b6843ca13febe4e0bacd390c8dbd&o=",
-                    Colors.blue,
-                    rate1),
-                hotels(
-                    "Radison blue",
-                    "https://cf.bstatic.com/xdata/images/hotel/square200/361538216.webp?k=24e0183d9cfd43ea9639dd23f9470bbe3ffc6be0afed3cfd3deba40da43d5dfe&o=",
-                    Colors.red,
-                    rate2),
-                hotels(
-                    "Sofitel hotel",
-                    "https://cf.bstatic.com/xdata/images/hotel/square200/330281530.webp?k=b2fc4dfb691c1c6c0d5cbe87d6190a260a55767cc069a79c5840e4dcd7093cd4&o=",
-                    Colors.green,
-                    rate3),
-                hotels(
-                    "Pullman hotel",
-                    "https://cf.bstatic.com/xdata/images/hotel/square200/251920769.webp?k=483c00c72f3868c63c862b5df003c2fad93a82a0faec4b95c367cba51ad581b7&o=",
-                    Colors.black,
-                    rate4),
-                hotels(
-                    "Novotel hotel",
-                    "https://cf.bstatic.com/xdata/images/hotel/square200/251611763.webp?k=e2415a01b1851d7f32c10b7bb0284a697f214afa18d423a902b69d88500a3796&o=",
-                    Colors.purple,
-                    rate5),
-                hotels(
-                    "Hotel ivotel",
-                    "https://cf.bstatic.com/xdata/images/hotel/square200/512905406.webp?k=9d12dc6fb5fd1bc1d7f4036c33f175f181069162dd6045d087870d719e1fd0cc&o=",
-                    Colors.orange,
-                    rate6)
+                Text(
+                  "Mouvement de la souris: $info",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      change ? a : b,
+                      height: 170,
+                      width: 170,
+                    ),
+                    Image.asset(
+                      change ? d : c,
+                      height: 170,
+                      width: 170,
+                    )
+                  ],
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (change = true) {
+                        onsubmit("un clique");
+                      } else {
+                        change = false;
+                      }
+                    });
+                  },
+                  onDoubleTap: () {
+                    setState(() {
+                      if (change = false) {
+                        onsubmit("double clique");
+                      } else {
+                        change = true;
+                      }
+                    });
+                  },
+                  onLongPress: () {
+                    setState(() {
+                      if (change = true) {
+                        onsubmit("long clique");
+                      } else {
+                        change = false;
+                      }
+                    });
+                  },
+                  onTapCancel: () {
+                    setState(() {
+                      if (change = false) {
+                        onsubmit("clique annulé");
+                      } else {
+                        change = true;
+                      }
+                    });
+                  },
+                  child: Center(
+                    child: Container(
+                      height: 50,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlueAccent,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        "VALIDATE",
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
-            )));
+            ),
+          ),
+        ));
   }
 }

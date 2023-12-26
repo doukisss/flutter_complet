782b53ef3ec4ea176cd0edfb36441ae415edc211
doukisss <doukissss@gmail.com>
2023-12-26 02:13:50 +0000

    Align Widget

--- lib/main.dart
+++ lib/main.dart
@@ -35,14 +35,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Animaux selectitem = choix[0];
-
-  void select(info) {
-    setState(() {
-      selectitem = info;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,72 +45,47 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(color: Colors.black)),
           actions: <Widget>[
             Icon(Icons.thumb_up, color: Colors.black),
-            PopupMenuButton(
-              icon: Icon(
-                Icons.menu,
-                size: 40,
-              ),
-              elevation: 14,
-              initialValue: choix[1],
-              onSelected: select,
-              itemBuilder: (BuildContext context) {
-                return choix.map((Animaux info) {
-                  return PopupMenuItem<Animaux>(
-                    child: Text(info.name),
-                    value: info,
-                  );
-                }).toList();
-              },
-            )
           ],
         ),
-        body: SingleChildScrollView(
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Affichage(info: selectitem)],
+            children: [
+              Align(
+                alignment: Alignment(0.5, 1),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+            ],
           ),
         ));
-  }
-}
-
-class Animaux {
-  String name;
-  String image;
-  Animaux({required this.name, required this.image});
-}
-
-List<Animaux> choix = <Animaux>[
-  Animaux(name: "Chat", image: "images/animaux/chat.jpg"),
-  Animaux(name: "Chien", image: "images/animaux/chien.jpg"),
-  Animaux(name: "Lion", image: "images/animaux/lion.jpg"),
-  Animaux(name: "Lapin", image: "images/animaux/lapin.jpg"),
-  Animaux(name: "Elephant", image: "images/animaux/elephant.jpg"),
-  Animaux(name: "Elephant", image: "images/animaux/elephant1.jpg"),
-  Animaux(name: "Pommes", image: "images/animaux/pommes.jpg"),
-];
-
-class Affichage extends StatelessWidget {
-  Animaux info;
-  Affichage({required this.info});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Column(
-          children: [
-            Text(
-              info.name,
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            Image.asset(
-              info.image,
-              height: 300,
-              width: 300,
-            )
-          ],
-        ),
-      ),
-    );
   }
 }

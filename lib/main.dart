6ed21f0e1c45546a9593b84d248dcdead33e135b
doukisss <doukissss@gmail.com>
2024-01-29 01:15:56 +0000

    FittedBox

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool opacite = true;
+  Widget hairimage(String img) {
+    return Container(
+      width: 600,
+      height: 600,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(65),
+          image: DecorationImage(image: NetworkImage(img))),
+    );
+  }
+
+  Widget description(String titre, String detail) {
+    return Container(
+      width: 450,
+      height: 150,
+      child: Column(children: [
+        Text(
+          titre,
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.green,
+          ),
+        ),
+        Text(
+          detail,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+            color: Colors.blue,
+          ),
+        )
+      ]),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,67 +82,69 @@ class _MyHomePageState extends State<MyHomePage> {
             Icon(Icons.thumb_up, color: Colors.black),
           ],
         ),
-        body: Stack(
-          fit: StackFit.expand,
+        body: ListView(
           children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("images/pizza.jpeg"),
-                      fit: BoxFit.cover)),
-            ),
             Padding(
-                padding: EdgeInsets.only(top: 50, left: 50),
-                child: AnimatedOpacity(
-                  opacity: opacite ? 1.0 : 0.0,
-                  duration: Duration(
-                    seconds: 2,
-                  ),
-                  child: Container(
-                    width: 250,
-                    height: 250,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            image: AssetImage("images/animaux/chat.jpg"),
-                            fit: BoxFit.contain)),
-                  ),
-                )),
-            Align(
-                alignment: Alignment.topCenter,
-                child: Opacity(
-                  opacity: opacite ? 1.0 : 0.5,
-                  child: Text(
-                    "OPACITY",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 60,
-                      color: Colors.white,
+              padding: EdgeInsets.all(10),
+              child: Container(
+                child: FittedBox(
+                  child: Material(
+                    borderRadius: BorderRadius.circular(65),
+                    elevation: 10,
+                    shadowColor: Colors.grey,
+                    child: Column(
+                      children: [
+                        hairimage(
+                            "https://coupedecheveuxhomme.org/wp-content/uploads/2018/03/13743438_1297114323633286_217607531_n1-1024x10241.jpg"),
+                        Padding(padding: EdgeInsets.all(5)),
+                        description("Modèle puntch",
+                            "Modèle puntch viub unjhn yubhion yuhojnh uhjioh uhui uhui uuh uu uiuj ujhio uihjih uihui uihunj hjn uijhi uihji uihuipjn uihjipuo uhjiopj uijiopj uiopj uij uiji ijiopj uiojpoij  ijju iojioj ioj iojpioj iji iojioj iojpiojpi "),
+                        SizedBox(
+                          height: 25,
+                          child: Container(width: 400, color: Colors.grey),
+                        ),
+                        hairimage(
+                            "https://coupedecheveuxhomme.org/wp-content/uploads/2018/03/26277352_212471045981885_3403692819653591040_n1-1068x12871.jpg"),
+                        Padding(padding: EdgeInsets.all(5)),
+                        description("Modèle puntch",
+                            "Modèle puntch viub unjhn yubhion yuhojnh uhjioh uhui uhui uuh uu uiuj ujhio uihjih uihui uihunj hjn uijhi uihji uihuipjn uihjipuo uhjiopj uijiopj uiopj uij uiji ijiopj uiojpoij  ijju iojioj ioj iojpioj iji iojioj iojpiojpi "),
+                        SizedBox(
+                          height: 25,
+                          child: Container(width: 400, color: Colors.grey),
+                        ),
+                        hairimage(
+                            "https://coupedecheveuxhomme.org/wp-content/uploads/2018/03/26870401_168054393812825_2093106480109584384_n1-1024x10011.jpg"),
+                        Padding(padding: EdgeInsets.all(5)),
+                        description("Modèle puntch",
+                            "Modèle puntch viub unjhn yubhion yuhojnh uhjioh uhui uhui uuh uu uiuj ujhio uihjih uihui uihunj hjn uijhi uihji uihuipjn uihjipuo uhjiopj uijiopj uiopj uij uiji ijiopj uiojpoij  ijju iojioj ioj iojpioj iji iojioj iojpiojpi "),
+                        SizedBox(
+                          height: 25,
+                          child: Container(width: 400, color: Colors.grey),
+                        ),
+                        hairimage(
+                            "https://coupedecheveuxhomme.org/wp-content/uploads/2018/03/13743438_1297114323633286_217607531_n1-1024x10241.jpg"),
+                        Padding(padding: EdgeInsets.all(5)),
+                        description("Modèle puntch",
+                            "Modèle puntch viub unjhn yubhion yuhojnh uhjioh uhui uhui uuh uu uiuj ujhio uihjih uihui uihunj hjn uijhi uihji uihuipjn uihjipuo uhjiopj uijiopj uiopj uij uiji ijiopj uiojpoij  ijju iojioj ioj iojpioj iji iojioj iojpiojpi "),
+                        SizedBox(
+                          height: 25,
+                          child: Container(width: 400, color: Colors.grey),
+                        ),
+                        hairimage(
+                            "https://coupedecheveuxhomme.org/wp-content/uploads/2018/03/13712203_133710833733602_863682278_n1-1024x10241.jpg"),
+                        Padding(padding: EdgeInsets.all(25)),
+                        description("Modèle puntch",
+                            "Modèle puntch viub unjhn yubhion yuhojnh uhjioh uhui uhui uuh uu uiuj ujhio uihjih uihui uihunj hjn uijhi uihji uihuipjn uihjipuo uhjiopj uijiopj uiopj uij uiji ijiopj uiojpoij  ijju iojioj ioj iojpioj iji iojioj iojpiojpi "),
+                        SizedBox(
+                          height: 25,
+                          child: Container(width: 400, color: Colors.grey),
+                        ),
+                      ],
                     ),
                   ),
-                )),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Opacity(
-                    opacity: opacite ? 1.0 : 0.5,
-                    child: OutlinedButton(
-                      onPressed: () {
-                        setState(() {
-                          opacite = !opacite;
-                        });
-                      },
-                      child: Text(
-                        "GO ON >>",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
-                          color: Colors.white,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ))),
+                ),
+              ),
+            )
           ],
         ));
   }

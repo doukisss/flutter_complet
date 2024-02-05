918770a165a951342225eeb843606e0fcac02166
doukisss <doukissss@gmail.com>
2024-02-05 08:42:16 +0000

    Transform

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: empty_constructor_bodies
 
+import 'dart:async';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -34,40 +37,74 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Widget womenhair(String holder, String img) {
+  int index = 0;
+  double rep = 0.0;
+  late Timer timer;
+  List<double> value = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), (Timer timer) {
+      setState(() {
+        rep = value[index];
+        index++;
+        if (index == value.length) {
+          index = 0;
+        }
+      });
+    });
+  }
+
+  Widget rotation() {
     return Container(
-      width: 200,
-      height: 300,
-      child: Column(children: [
-        ClipRRect(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(75),
-            topRight: Radius.circular(75),
-          ),
-          child: FadeInImage.assetNetwork(
-            placeholder: holder,
-            image: img,
-            fit: BoxFit.fill,
-            fadeInDuration: Duration(
-              seconds: 5,
-            ),
-          ),
+        child: Transform.rotate(
+      angle: rep,
+      origin: Offset(0.0, 0.0),
+      child: Image(
+        image: AssetImage("images/terre4.png"),
+        width: 150,
+        height: 150,
+      ),
+    ));
+  }
+
+  Widget scale() {
+    return Container(
+      child: Transform.scale(
+        scale: rep == 0 ? 0 : rep / 50,
+        origin: Offset(0.0, 0.0),
+        child: Image(
+          image: AssetImage("images/soleil1.jpeg"),
+          width: 150,
+          height: 150,
         ),
-      ]),
+      ),
     );
   }
 
-  Widget titre(String info) {
+  Widget tranlate() {
     return Container(
-      width: 200,
-      child: Column(
-        children: [
-          Text(
-            info,
-            style: TextStyle(
-                color: Colors.blue, fontSize: 25, fontWeight: FontWeight.bold),
-          )
-        ],
+      child: Transform.translate(
+        offset: Offset(rep, -rep * 4),
+        child: Image(
+          image: AssetImage("images/fusée3.jpg"),
+          width: 150,
+          height: 150,
+        ),
+      ),
+    );
+  }
+
+  Widget skew() {
+    return Container(
+      child: Transform(
+        transform: Matrix4.skewX(rep / 100),
+        child: Image(
+          image: AssetImage("images/satellite3.jpeg"),
+          width: 150,
+          height: 150,
+        ),
       ),
     );
   }
@@ -84,135 +121,23 @@ class _MyHomePageState extends State<MyHomePage> {
             Icon(Icons.thumb_up, color: Colors.black),
           ],
         ),
-        body: ListView(
-          children: [
-            FittedBox(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        womenhair("images/Fidget-spinner.gif",
-                            "https://i.pinimg.com/564x/0a/81/a2/0a81a2db05859cb9a9d1896d556ef769.jpg"),
-                        womenhair("images/Spinner-3.gif",
-                            "https://i.pinimg.com/564x/73/dd/d3/73ddd32402246caeb93211a5cb2b9a18.jpg"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        titre("Rat Congo"),
-                        titre("Classe"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        womenhair("images/Fidget-spinner.gif",
-                            "https://i.pinimg.com/474x/99/1d/e8/991de80a9cde10a0a51d10b4b1b82c09.jpg"),
-                        womenhair("images/Spinner-3.gif",
-                            "https://i.pinimg.com/236x/41/81/42/41814220b9db470a0f21ab45e3d2b786.jpg"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        titre("Nature frisé"),
-                        titre("création salon"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        womenhair("images/Fidget-spinner.gif",
-                            "https://i.pinimg.com/474x/99/1d/e8/991de80a9cde10a0a51d10b4b1b82c09.jpg"),
-                        womenhair("images/Spinner-3.gif",
-                            "https://i.pinimg.com/236x/41/81/42/41814220b9db470a0f21ab45e3d2b786.jpg"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        titre("Nature frisé"),
-                        titre("création salon"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        womenhair("images/Fidget-spinner.gif",
-                            "https://i.pinimg.com/564x/0a/81/a2/0a81a2db05859cb9a9d1896d556ef769.jpg"),
-                        womenhair("images/Spinner-3.gif",
-                            "https://i.pinimg.com/564x/73/dd/d3/73ddd32402246caeb93211a5cb2b9a18.jpg"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        titre("Rat Congo"),
-                        titre("Classe"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        womenhair("images/Fidget-spinner.gif",
-                            "https://i.pinimg.com/474x/99/1d/e8/991de80a9cde10a0a51d10b4b1b82c09.jpg"),
-                        womenhair("images/Spinner-3.gif",
-                            "https://i.pinimg.com/236x/41/81/42/41814220b9db470a0f21ab45e3d2b786.jpg"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        titre("Nature frisé"),
-                        titre("création salon"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        womenhair("images/Fidget-spinner.gif",
-                            "https://i.pinimg.com/474x/99/1d/e8/991de80a9cde10a0a51d10b4b1b82c09.jpg"),
-                        womenhair("images/Spinner-3.gif",
-                            "https://i.pinimg.com/236x/41/81/42/41814220b9db470a0f21ab45e3d2b786.jpg"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        titre("Nature frisé"),
-                        titre("création salon"),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
+        body: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Colors.black,
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                ),
+                scale(),
+                rotation(),
+                skew(),
+                tranlate(),
+              ],
+            ),
+          ),
         ));
   }
 }

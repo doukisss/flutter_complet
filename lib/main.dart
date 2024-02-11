4d5a6eb8ce0c10ffb482a768222528a512c92493
doukisss <doukissss@gmail.com>
2024-02-11 15:24:54 +0000

    Graphics

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: empty_constructor_bodies
 
-import 'dart:async';
-import 'dart:ffi';
+import 'package:charts_flutter_new/flutter.dart' as charts;
 
 import 'package:flutter/material.dart';
 
@@ -37,107 +36,90 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int index = 0;
-  double rep = 0.0;
-  late Timer timer;
-  List<double> value = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-
-  @override
-  void initState() {
-    super.initState();
-    Timer.periodic(Duration(seconds: 1), (Timer timer) {
-      setState(() {
-        rep = value[index];
-        index++;
-        if (index == value.length) {
-          index = 0;
-        }
-      });
-    });
-  }
-
-  Widget rotation() {
-    return Container(
-        child: Transform.rotate(
-      angle: rep,
-      origin: Offset(0.0, 0.0),
-      child: Image(
-        image: AssetImage("images/terre4.png"),
-        width: 150,
-        height: 150,
-      ),
-    ));
-  }
-
-  Widget scale() {
-    return Container(
-      child: Transform.scale(
-        scale: rep == 0 ? 0 : rep / 50,
-        origin: Offset(0.0, 0.0),
-        child: Image(
-          image: AssetImage("images/soleil1.jpeg"),
-          width: 150,
-          height: 150,
-        ),
-      ),
-    );
-  }
-
-  Widget tranlate() {
-    return Container(
-      child: Transform.translate(
-        offset: Offset(rep, -rep * 4),
-        child: Image(
-          image: AssetImage("images/fusée3.jpg"),
-          width: 150,
-          height: 150,
-        ),
-      ),
-    );
-  }
-
-  Widget skew() {
-    return Container(
-      child: Transform(
-        transform: Matrix4.skewX(rep / 100),
-        child: Image(
-          image: AssetImage("images/satellite3.jpeg"),
-          width: 150,
-          height: 150,
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
+    List<PowerCountry> datacountry = [
+      PowerCountry("USA", 329665656, Colors.red),
+      PowerCountry("Chine", 1433675765, Colors.blue),
+      PowerCountry("Russie", 145872256, Colors.green),
+      PowerCountry("Allemagne", 83517045, Colors.orange),
+      PowerCountry("UK", 67530172, Colors.pink),
+      PowerCountry("Japon", 126860301, Colors.grey),
+      PowerCountry("Canada", 37411045, Colors.cyanAccent),
+    ];
+
+    var series = [
+      charts.Series(
+        domainFn: (PowerCountry PowerCountry, _) => PowerCountry.country,
+        measureFn: (PowerCountry PowerCountry, _) => PowerCountry.population,
+        colorFn: (PowerCountry PowerCountry, _) => PowerCountry.color,
+        id: "PowerCountry",
+        data: datacountry,
+        labelAccessorFn: (PowerCountry PowerCountry, _) =>
+            "${PowerCountry.country}: ${PowerCountry.population.toString()}",
+      )
+    ];
+
+    var chart = charts.BarChart(
+      series,
+      vertical: false,
+      barRendererDecorator: charts.BarLabelDecorator<String>(),
+    );
+
+    // var chart1 =new charts.PieChart(
+    //   series,
+    //   defaultRenderer:new charts.ArcRendererConfig(
+    //     arcRendererDecorators: [new charts.ArcLabelDecorator()],
+    //     arcWidth: 80,
+    //   ),
+    //   animate: true,
+    // );
+
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.purpleAccent,
-          title: const Text("Tutoriel flutter",
-              style: TextStyle(color: Colors.black)),
-          actions: <Widget>[
-            Icon(Icons.thumb_up, color: Colors.black),
-          ],
-        ),
+        // appBar: AppBar(
+        //   centerTitle: true,
+        //   backgroundColor: Colors.purpleAccent,
+        //   title: const Text("Tutoriel flutter",
+        //       style: TextStyle(color: Colors.black)),
+        //   actions: <Widget>[
+        //     Icon(Icons.thumb_up, color: Colors.black),
+        //   ],
+        // ),
         body: SingleChildScrollView(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            color: Colors.black,
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                ),
-                scale(),
-                rotation(),
-                skew(),
-                tranlate(),
-              ],
-            ),
+            child: Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
           ),
-        ));
+          const Text(
+            "The powerfull countries",
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.orange),
+          ),
+          SizedBox(
+            height: 275,
+            child: chart,
+          ),
+          // SizedBox(
+          //   height: 275,
+          //   child: chart1,
+          // ),
+        ],
+      ),
+    )));
   }
+}
+
+class PowerCountry {
+  final String country;
+  final int population;
+  final charts.Color color;
+
+  PowerCountry(this.country, this.population, Color color)
+      : this.color = charts.Color(
+            g: color.green, r: color.red, a: color.alpha, b: color.blue);
 }

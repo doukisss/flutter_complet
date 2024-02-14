651b67b17dfcda91f8d89b47a745b78103850b86
doukisss <doukissss@gmail.com>
2024-02-14 17:54:17 +0000

    Dismissible

--- lib/main.dart
+++ lib/main.dart
@@ -36,90 +36,163 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<InfoList> execute = [
+    InfoList("images/boisson.jpeg", "Sprite", "13/02/2024", Colors.orange),
+    InfoList("images/pizza.jpeg", "Pizza", "13/02/2024", Colors.orange),
+    InfoList("images/humburger.jpeg", "Humburger", "13/02/2024", Colors.orange),
+    InfoList(
+        "images/frites_a_la_viande.jpeg", "Frite", "13/02/2024", Colors.orange),
+    InfoList("images/fusée3.jpg", "Fusée", "13/02/2024", Colors.orange),
+    InfoList("images/poulet.jpeg", "Poulet", "13/02/2024", Colors.orange),
+    InfoList("images/spaguetti.jpeg", "Spaguetti", "13/02/2024", Colors.orange),
+    InfoList("images/viande.jpeg", "Viande", "13/02/2024", Colors.orange),
+    InfoList("images/poisson_braisé.jpeg", "Poisson braisé", "13/02/2024",
+        Colors.orange),
+    InfoList("images/salade_composé.jpeg", "Salade composé", "13/02/2024",
+        Colors.orange),
+  ];
   @override
   Widget build(BuildContext context) {
-    List<PowerCountry> datacountry = [
-      PowerCountry("USA", 329665656, Colors.red),
-      PowerCountry("Chine", 1433675765, Colors.blue),
-      PowerCountry("Russie", 145872256, Colors.green),
-      PowerCountry("Allemagne", 83517045, Colors.orange),
-      PowerCountry("UK", 67530172, Colors.pink),
-      PowerCountry("Japon", 126860301, Colors.grey),
-      PowerCountry("Canada", 37411045, Colors.cyanAccent),
-    ];
-
-    var series = [
-      charts.Series(
-        domainFn: (PowerCountry PowerCountry, _) => PowerCountry.country,
-        measureFn: (PowerCountry PowerCountry, _) => PowerCountry.population,
-        colorFn: (PowerCountry PowerCountry, _) => PowerCountry.color,
-        id: "PowerCountry",
-        data: datacountry,
-        labelAccessorFn: (PowerCountry PowerCountry, _) =>
-            "${PowerCountry.country}: ${PowerCountry.population.toString()}",
-      )
-    ];
-
-    var chart = charts.BarChart(
-      series,
-      vertical: false,
-      barRendererDecorator: charts.BarLabelDecorator<String>(),
-    );
-
-    // var chart1 =new charts.PieChart(
-    //   series,
-    //   defaultRenderer:new charts.ArcRendererConfig(
-    //     arcRendererDecorators: [new charts.ArcLabelDecorator()],
-    //     arcWidth: 80,
-    //   ),
-    //   animate: true,
-    // );
-
     return Scaffold(
-        // appBar: AppBar(
-        //   centerTitle: true,
-        //   backgroundColor: Colors.purpleAccent,
-        //   title: const Text("Tutoriel flutter",
-        //       style: TextStyle(color: Colors.black)),
-        //   actions: <Widget>[
-        //     Icon(Icons.thumb_up, color: Colors.black),
-        //   ],
-        // ),
-        body: SingleChildScrollView(
-            child: Padding(
-      padding: EdgeInsets.only(top: 20),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          const Text(
-            "The powerfull countries",
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.orange),
-          ),
-          SizedBox(
-            height: 275,
-            child: chart,
-          ),
-          // SizedBox(
-          //   height: 275,
-          //   child: chart1,
-          // ),
-        ],
-      ),
-    )));
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.purpleAccent,
+          title: const Text("Tutoriel flutter",
+              style: TextStyle(color: Colors.black)),
+          actions: <Widget>[
+            Icon(Icons.thumb_up, color: Colors.black),
+          ],
+        ),
+        body: ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: execute.length,
+            itemBuilder: ((context, index) {
+              InfoList ls = execute[index];
+              return Dismissible(
+                key: Key(ls.detail),
+                child: WidgetInfo(
+                  entry: execute[index],
+                  key: Key(ls.detail),
+                ),
+                onDismissed: (direction) {
+                  setState(() {
+                    execute.removeAt(index);
+                  });
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(
+                      ls.detail + " supprimé",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    action: SnackBarAction(
+                        label: "Annuler",
+                        onPressed: () {
+                          setState(() {
+                            execute.add(ls);
+                          });
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(
+                              ls.detail + " rajouté en fin de liste",
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
+                          ));
+                        }),
+                  ));
+                },
+                background: Container(
+                  color: ls.color,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.delete,
+                        size: 25,
+                      ),
+                      Text(
+                        ls.detail + " sera supprimé.",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                secondaryBackground: Container(
+                  color: ls.color,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Icon(
+                        Icons.delete_forever,
+                        size: 25,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            })));
   }
 }
 
-class PowerCountry {
-  final String country;
-  final int population;
-  final charts.Color color;
+class InfoList {
+  String image;
+  String detail;
+  String date;
+  Color color;
+  InfoList(this.image, this.detail, this.date, this.color);
+}
 
-  PowerCountry(this.country, this.population, Color color)
-      : this.color = charts.Color(
-            g: color.green, r: color.red, a: color.alpha, b: color.blue);
+class WidgetInfo extends StatelessWidget {
+  final InfoList entry;
+
+  const WidgetInfo({super.key, required this.entry});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(2),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image.asset(
+                  entry.image,
+                  width: 70,
+                  height: 70,
+                ),
+                Text(
+                  entry.detail,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  entry.date,
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 30,
+                  width: 2,
+                  child: Container(
+                    color: entry.color,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: SizedBox(
+              height: 2,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }

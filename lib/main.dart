9034ad21dcb15c0096d3675e186ae447c13aed00
doukisss <doukissss@gmail.com>
2024-02-21 12:35:56 +0000

    AnimatedPadding

--- lib/main.dart
+++ lib/main.dart
@@ -36,8 +36,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double width = 110;
-  double height = 140;
+  double padding = 100;
+  Widget element(String image, Curve curve) {
+    return AnimatedPadding(
+      padding: EdgeInsets.all(padding),
+      duration: Duration(
+        seconds: 1,
+      ),
+      curve: curve,
+      child: Image.asset(
+        image,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,123 +63,104 @@ class _MyHomePageState extends State<MyHomePage> {
         //   ],
         // ),
         body: Container(
-      decoration: BoxDecoration(
-        color: Colors.green,
-      ),
       child: Column(
         children: [
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset(
-                  "images/f1.jpg",
-                  width: 110,
-                  height: 140,
+          Stack(
+            children: [
+              Container(
+                height: 300,
+                width: MediaQuery.of(context).size.width,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(60),
+                    bottomRight: Radius.circular(60),
+                  ),
+                  child: Image(
+                    image: AssetImage("images/f9.jpg"),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                Image.asset(
-                  "images/f1.jpg",
-                  width: 110,
-                  height: 140,
+              ),
+              Positioned(
+                top: 60,
+                right: 10,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [Icon(Icons.add_shopping_cart, size: 60)],
                 ),
-              ],
-            ),
+              ),
+              Positioned(
+                width: 25,
+                height: 25,
+                top: 60,
+                right: 10,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.red,
+                  ),
+                  child: Text(
+                    "3",
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Positioned(
+                  bottom: 10,
+                  right: 30,
+                  child: Row(
+                    children: [
+                      FloatingActionButton(
+                        child: Icon(Icons.remove),
+                        backgroundColor: Colors.blue,
+                        onPressed: () {
+                          setState(() {
+                            if (padding <
+                                MediaQuery.of(context).size.width / 2 - 25) {
+                              padding += 25;
+                            }
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      FloatingActionButton(
+                        child: Icon(Icons.add),
+                        backgroundColor: Colors.blue,
+                        onPressed: () {
+                          setState(() {
+                            if (padding > 25) {
+                              padding -= 25;
+                            }
+                          });
+                        },
+                      ),
+                    ],
+                  ))
+            ],
           ),
           Expanded(
             child: Container(
               width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(60),
-                  topRight: Radius.circular(60),
-                ),
-              ),
+              height: 300,
               child: SingleChildScrollView(
                 scrollDirection: Axis.vertical,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Text(
-                        "FLEURISTE BIO",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.green,
-                        ),
-                      ),
-                    ),
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              OutlinedButton(
-                                onPressed: () {
-                                  setState(() {
-                                    width += 100;
-                                    height += 100;
-                                  });
-                                },
-                                child: Icon(
-                                  Icons.zoom_in,
-                                  size: 30,
-                                ),
-                              ),
-                              AnimatedContainer(
-                                duration: Duration(seconds: 1),
-                                curve: Curves.bounceInOut,
-                                child: Image.asset(
-                                  "images/f1.jpg",
-                                  width: width,
-                                  height: height,
-                                ),
-                              ),
-                              OutlinedButton(
-                                onPressed: () {
-                                  setState(() {
-                                    if (width > 100 || height > 100) {
-                                      width -= 100;
-                                      height -= 100;
-                                    }
-                                  });
-                                },
-                                child: Icon(
-                                  Icons.zoom_out,
-                                  size: 30,
-                                ),
-                              )
-                            ],
-                          ),
-                          Image.asset(
-                            "images/f1.jpg",
-                            width: 110,
-                            height: 140,
-                          ),
-                          Image.asset(
-                            "images/f1.jpg",
-                            width: 110,
-                            height: 140,
-                          ),
-                          Image.asset(
-                            "images/f1.jpg",
-                            width: 110,
-                            height: 140,
-                          ),
-                          Image.asset(
-                            "images/f1.jpg",
-                            width: 110,
-                            height: 140,
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                child: Column(children: [
+                  element("images/f1.jpg", Curves.bounceIn),
+                  element("images/f2.jpg", Curves.bounceInOut),
+                  element("images/f3.jpg", Curves.bounceOut),
+                  element("images/f4.jpg", Curves.decelerate),
+                  element("images/f5.jpg", Curves.linear),
+                  element("images/f6.jpg", Curves.ease),
+                  element("images/f7.jpg", Curves.easeInOut),
+                  element("images/f8.jpg", Curves.elasticInOut),
+                  element("images/f9.jpg", Curves.easeOutCirc),
+                ]),
               ),
             ),
           )
